7e44a99153c9c060fa8544fe91835eb1b5cfc809
jjin0ee <jjin0ee@littlefox.com>
2023-01-16 13:01:02 +0900

    sstyle component를 이요해 비디오 배너 생성

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import axios from "../api/axios";
 import requests from "../api/requests";
 import "./Banner.css";
 
 export default function Banner() {
     const [movie, setMovie] = useState([]);
+    const [isClicked, setIsClicked] = useState(false);
 
     useEffect(() => {
         fetchData();
@@ -14,7 +16,7 @@ export default function Banner() {
         //현재 상영중인 영화 정보를 가져오기
         const request = await axios.get(requests.fetchNowPlaying);
         
-        // 여러 영화 중 여화 하나의 ID 가져오기
+        // 여러 영화 중 dud화 하나의 ID 가져오기
         const movieId = request.data.results[
             Math.floor(Math.random() * request.data.results.length)
         ].id;
@@ -31,28 +33,78 @@ export default function Banner() {
     const truncate = (str, n) => {
         return str?.length > n ? str.substr(0, n - 1) + "..." : str;
     }
-
-    return (
-        <header
-            className='banner'
-            style={{
-                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-                backgroundPosition: "top center",
-                backgroundSize: "cover"
-            }}
-        >
-
-        <div className='banner__contents'>
-            <h1>{movie.title || movie.name || movie.original_name}</h1>
-            <div className='banner__buttons'>
-                <button className='banner__button play'>Play</button>
-                <button className='banner__button info'>More Information</button>
+    
+    if(isClicked){
+        return (
+            <Container>
+                <HomeContainer>
+                <Iframe
+                    width="640"
+                    height="360"
+                    src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="autoplay; fullscreen"
+                    allowfullscreen
+                ></Iframe>
+                </HomeContainer>
+            </Container>
+        )
+    }else{
+        return (
+            <header
+                className='banner'
+                style={{
+                    backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+                    backgroundPosition: "top center",
+                    backgroundSize: "cover"
+                }}
+            >
+    
+            <div className='banner__contents'>
+                <h1>{movie.title || movie.name || movie.original_name}</h1>
+                <div className='banner__buttons'>
+                    <button className='banner__button play' onClick={() => setIsClicked(true)}>Play</button>
+                    <button className='banner__button info'>More Information</button>
+                </div>
+    
+                <h1 className='banner__description'>{truncate(movie.overview, 100)}</h1>
             </div>
-
-            <h1 className='banner__description'>{truncate(movie.overview, 100)}</h1>
-        </div>
-        
-        <div className='banner--fadeBottom'></div>
-        </header>
-    )
+            
+            <div className='banner--fadeBottom'></div>
+            </header>
+        )
+    }
 }
+
+const Iframe = styled.iframe`
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    opacity: 0.65;
+    border: none;
+
+    &::after {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+`;
+
+const HomeContainer = styled.div`
+    width: 100%;
+    height: 100%;
+`;
