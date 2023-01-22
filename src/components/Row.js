fdb42b9200f3a8587e3ebedf84a0aceba18eb353
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2023-01-22 21:47:53 +0900

    모달 ui

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,54 +1,71 @@
 import axios from "../api/axios";
-import React, { useEffect, useState } from 'react'
-import "./Row.css"
+import React, { useEffect, useState } from "react";
+import "./Row.css";
+import MovieModal from "./MovieModal";
 
-export default function Row({ isLargeRow, title, id, fetchUrl}) {
-    const [movies, setMovies] = useState([]);
+export default function Row({ isLargeRow, title, id, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
 
-    useEffect(() => {
-        fetchMovieData();
-    }, []);
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
 
-    const fetchMovieData = async () => {
-        const request = await axios.get(fetchUrl);
-        setMovies(request.data.results);
-    }
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+    setMovies(request.data.results);
+  };
 
-    return (
-        <section className="row">
-            {/** TITLE */}
-            <h2>{title}</h2>
-            <div className="slider">
-                <div  className="slider__arrow-left">
-                    <span className="arrow"
-                        onClick={() => {
-                            document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-                        }}
-                    >
-                        {"<"}
-                    </span>
-                </div>
-                <div id={id} className="row__posters">
-                    {movies.map(movie => (
-                        <img
-                            key={movie.id}
-                            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                            src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-                            loading="lazy"
-                            alt={movie.name}
-                        />
-                    ))}
-                </div>
-                <div  className="slider__arrow-right">
-                    <span className="arrow"
-                        onClick={() => {
-                            document.getElementById(id).scrollLeft += window.innerWidth - 80;
-                        }}
-                    >
-                        {">"}
-                    </span>
-                </div>
-            </div>
-        </section>
-    )
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
+
+  return (
+    <section className="row">
+      {/** TITLE */}
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {"<"}
+          </span>
+        </div>
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`https://image.tmdb.org/t/p/original/${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              loading="lazy"
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        <div className="slider__arrow-right">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >
+            {">"}
+          </span>
+        </div>
+      </div>
+
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
+    </section>
+  );
 }
