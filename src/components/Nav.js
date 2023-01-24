fdbbd726af0e91763b83adb86a2e06c1a9299570
parkjeanyoung <jean.young.park.96@gmail.com>
2023-01-24 17:48:27 +0900

    검색 구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./Nav.css";
 
 export default function Nav() {
 
     const [show, setShow] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+    const navigate = useNavigate();
 
     // 빈 배열일 때는 컴포넌트가 마운트 될 때만 실행됨
     useEffect(() => {
         window.addEventListener("scroll",() => {
-            console.log(window.scrollY);
             if(window.scrollY > 50){
                 setShow(true);
             }else{
@@ -21,6 +23,11 @@ export default function Nav() {
         };
     }, []);
 
+    const handleChange = (e) => {
+        setSearchValue(e.target.value);
+        navigate(`/search?q=${e.target.value}`);
+    };
+
     return (
     <nav className={`nav ${show && 'nav__black'}`}>
         <img
@@ -29,6 +36,9 @@ export default function Nav() {
             className='nav__logo'
             onClick={() => window.location.reload()}
         />
+
+        <input value={searchValue} onChange={handleChange} className="nav__input" type="text" placeholder="영화를 검색해주세요." />
+
         <img
             alt='User logged'
             src='https://via.placeholder.com/150'
--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
   // 개발 환경에서만 동작
   // 문제를 출력하는데 사용됨
   // <React.StrictMode>
-    <App />
   // </React.StrictMode>
+  <BrowserRouter>
+    <App />
   </BrowserRouter>
 );
 
